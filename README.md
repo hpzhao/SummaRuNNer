85006e9c5b8be5b18798a93f560211e74860c1d3
hpzhao <huaipengzhao@gmail.com>
2018-04-09 07:23:45 +0800

    update

--- README.md
+++ README.md
@@ -30,12 +30,16 @@ python main.py -test -model RNN -load_dir checkpoints/XXX.pt
 
 ### Download Data:  
 
-+ 百度云:[https://pan.baidu.com/s/1jR77aWQDzw-pTgOjrSnszQ](https://pan.baidu.com/s/1jR77aWQDzw-pTgOjrSnszQ) 密码:`8t6v`
++ 百度云:[https://pan.baidu.com/s/1LV3iuuH1NjxuAJd0iz14lA](https://pan.baidu.com/s/1LV3iuuH1NjxuAJd0iz14lA) 密码:`ivzl`
 
-+ Google Driver:[data.tar.gz](https://drive.google.com/file/d/1TbeY58G5jCEsj4ZakadR-dM1dfYHMein/view?usp=sharing)
++ Google Driver:[data.tar.gz](https://drive.google.com/file/d/1JgsboIAs__r6XfCbkDWgmberXJw8FBWE/view?usp=sharing)
 
 + Source Data:[Neural Summarization by Extracting Sentences and Words](https://docs.google.com/uc?id=0B0Obe9L1qtsnSXZEd0JCenIyejg&export=download)
 
 ### Evaluation
 
 + [Tools](https://github.com/hpzhao/nlp-metrics)
+
+### Acknowledge
+
+Thanks for @[AlJohri](https://github.com/AlJohri)'s contribution
--- preprocess.py
+++ preprocess.py
@@ -1,27 +1,27 @@
-#!/usr/bin/env python
-#coding:utf8
 import argparse
-import torch
-from utils import Vocab,Dataset
-from tqdm import tqdm
+import json
+import numpy as np
+from collections import OrderedDict
 from glob import glob
 from time import time
-PAD_IDX = 0
-UNK_IDX = 1
-PAD_TOKEN = 'PAD_TOKEN'
-UNK_TOKEN = 'UNK_TOKEN'
+from multiprocessing import Pool,cpu_count
+from itertools import chain
 
 def build_vocab(args):
+    print('start building vocab')
+
+    PAD_IDX = 0
+    UNK_IDX = 1
+    PAD_TOKEN = 'PAD_TOKEN'
+    UNK_TOKEN = 'UNK_TOKEN'
+    
     f = open(args.embed)
     embed_dim = int(next(f).split()[1])
 
-    word2id = {}
-    id2word = {}
+    word2id = OrderedDict()
     
     word2id[PAD_TOKEN] = PAD_IDX
     word2id[UNK_TOKEN] = UNK_IDX
-    id2word[PAD_IDX] = PAD_TOKEN
-    id2word[UNK_IDX] = UNK_TOKEN
     
     embed_list = []
     # fill PAD and UNK vector
@@ -29,26 +29,24 @@ def build_vocab(args):
     embed_list.append([0 for _ in range(embed_dim)])
     
     # build Vocab
-
     for line in f:
         tokens = line.split()
-        word = tokens[0]
-        vector = [float(num) for num in tokens[1:]]
-        
+        word = tokens[:-1*embed_dim][0]
+        vector = [float(num) for num in tokens[-1*embed_dim:]]
         embed_list.append(vector)
         word2id[word] = len(word2id)
-        id2word[len(id2word)] = word
-    
-    embed = torch.FloatTensor(embed_list)
-    vocab = Vocab(embed, word2id, id2word)
-    torch.save(vocab,args.vocab)
+    f.close()
+    embed = np.array(embed_list,dtype=np.float32)
+    np.savez_compressed(file=args.vocab, embedding=embed)
+    with open(args.word2id,'w') as f:
+        json.dump(word2id,f)
 
-def build_dataset(args):
+def worker(files):
     examples = []
-    for f in tqdm(glob(args.source_dir)):
-        parts = open(f).read().split('\n\n')
+    for f in files:
+        parts = open(f,encoding='latin-1').read().split('\n\n')
         try:
-            entities = { line.strip().split(':')[0]:line.strip().split(':')[1] for line in parts[-1].split('\n')}
+            entities = { line.strip().split(':')[0]:line.strip().split(':')[1].lower() for line in parts[-1].split('\n')}
         except:
             continue
         sents,labels,summaries = [],[],[]
@@ -72,17 +70,47 @@ def build_dataset(args):
             summaries.append(line)
         ex = {'doc':'\n'.join(sents),'labels':'\n'.join(labels),'summaries':'\n'.join(summaries)}
         examples.append(ex)
-    dataset = Dataset(examples)
-    torch.save(dataset,args.target_dir)
+    return examples
+
+def build_dataset(args):
+    t1 = time()
+    
+    print('start building dataset')
+    if args.worker_num == 1 and cpu_count() > 1:
+        print('[INFO] There are %d CPUs in your device, please increase -worker_num to speed up' % (cpu_count()))
+        print("       It's a IO intensive application, so 2~10 may be a good choise")
+
+    files = glob(args.source_dir)
+    data_num = len(files)
+    group_size = data_num // args.worker_num
+    groups = []
+    for i in range(args.worker_num):
+        if i == args.worker_num - 1:
+            groups.append(files[i*group_size : ])
+        else:
+            groups.append(files[i*group_size : (i+1)*group_size])
+    p = Pool(processes=args.worker_num)
+    multi_res = [p.apply_async(worker,(fs,)) for fs in groups]
+    res = [res.get() for res in multi_res]
+    
+    with open(args.target_dir,'w',encoding='utf8') as f:
+        for row in chain(*res):
+            f.write(json.dumps(row, ensure_ascii=False) + "\n")
+
+    t2 = time()
+    print('Time Cost : %.1f seconds' % (t2 - t1))
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('-embed', type=str, default='data/100.w2v')
-    parser.add_argument('-vocab', type=str, default='data/vocab.pt')
-    parser.add_argument('-source_dir', type=str, default='data/neuralsum/dailymail/test/*')
-    parser.add_argument('-target_dir', type=str, default='data/test.pt')
+    
     parser.add_argument('-build_vocab',action='store_true')
+    parser.add_argument('-embed', type=str, default='data/100.w2v')
+    parser.add_argument('-vocab', type=str, default='data/embedding.npz')
+    parser.add_argument('-word2id',type=str,default='data/word2id.json')
+
+    parser.add_argument('-worker_num',type=int,default=1)
+    parser.add_argument('-source_dir', type=str, default='data/neuralsum/dailymail/validation/*')
+    parser.add_argument('-target_dir', type=str, default='data/val.json')
 
     args = parser.parse_args()
     
